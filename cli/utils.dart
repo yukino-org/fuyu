5e1fe7b9f4596ded8e3d1a29e8e1bd3e0081b1b9
zyrouge <naveencnaveen96@gmail.com>
2022-03-13 12:33:09 +0530

    ci: add release

--- cli/utils.dart
+++ cli/utils.dart
@@ -1,8 +1,24 @@
 import 'dart:io';
 import 'package:path/path.dart' as path;
 
+export '../src/config/meta.dart';
+
 abstract class Paths {
   static final String rootDir = Directory.current.path;
-
   static final String srcDir = path.join(rootDir, 'src');
+  static final String mainFilePath = path.join(srcDir, 'main.dart');
+  static final String outputDir = path.join(rootDir, 'bin');
+}
+
+enum OperatingSystem {
+  windows,
+  linux,
+  macos,
+}
+
+OperatingSystem getOperatingSystem() {
+  if (Platform.isWindows) return OperatingSystem.windows;
+  if (Platform.isLinux) return OperatingSystem.linux;
+  if (Platform.isMacOS) return OperatingSystem.macos;
+  throw Exception('Unsupported platform');
 }
